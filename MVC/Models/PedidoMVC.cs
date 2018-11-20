8de25dd10a8079164d7ef5a9ebf196c5f7988b46
Diego <diego.fmm96@gmail.com>
2018-11-20 11:33:00 -0200

    Trabalho ELIZEU ADS 4
    (0,1) Crie a solução com seu nome (ex. ElizeuFachini)
    (0,1) Crie o projeto Web API
    a.	(0,1) - Crie o modelo Pedido
    b.	(0,2) - Inclua os atributos IdPedido, NomeMedicamento, QtdeMedicamento, CodCliente e DataEntrega.
    c.	(0,1) – Defina o IdPedido como Chave primária
    d.	(0,2) – Crie o PedidoContext
    e.	(0,3) – Crie o Controller do API com EntityFramework
    (0,1) Crie o Projeto MVC empty
    a.	(0,2) – Crie a GlobalVariable
    b.	(0,1) – Crie um controlador Home
    
    c.	(0,2) – Crie o modelo MVC igual ao Pedido da API
    (0,1) Defina a chave primária para o IdPedido
    (0,1) Coloque um nome amigável para ser exibido (DisplayName)
    (0,2) Defina como obrigatório NomeMedicamento, QtdeMedicamento, CodCliente e DataEntrega
    (0,1) Usando o DataType, defina o DataEntrega como tipo Date
    d.	(0,2) – Crie na Index a Lista dos pedidos
    e.	(0,2) – Crie o método e view de adicionar pedido
    f.	(0,2) – Crie o método e view de editar pedido
    g.	(0,2) – Crie o método de excluir um pedido

---
+++ MVC/Models/PedidoMVC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class PedidoMVC
+    {
+        [Key]
+        public int IdPedido { get; set; }
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Nome do medicamento obrigatório", AllowEmptyStrings = false)]
+        public string NomeMedicamento { get; set; }
+
+        [Display(Name = "Quantidade")]
+        [Required(ErrorMessage = "Quantidade obrigatória", AllowEmptyStrings = false)]
+        public string QtdeMedicamento { get; set; }
+
+        [Display(Name = "Código Cliente")]
+        [Required(ErrorMessage = "Código do cliente obrigatório", AllowEmptyStrings = false)]
+        public string CodCliente { get; set; }
+
+        [Display(Name = "Data de entrega")]
+        [Required(ErrorMessage = "Data de entrega obrigatória", AllowEmptyStrings = false)]
+        [DataType(DataType.Date)]
+        public string DataEntrega { get; set; }
+    }
+}
